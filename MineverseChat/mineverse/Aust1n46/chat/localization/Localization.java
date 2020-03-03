c6faba8c2a4a8b0cf9f119046c79364c313cbbd7
Aust1n46 <aust1n46@yahoo.com>
2020-03-03 18:05:47 -0500

    Uncomment localization imports.

--- MineverseChat/mineverse/Aust1n46/chat/localization/Localization.java
+++ MineverseChat/mineverse/Aust1n46/chat/localization/Localization.java
@@ -9,38 +9,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 //This class is used to create objects of localization for different languages.
 public class Localization { 
-	//private FileConfiguration localization;
-	//private File localizationFile;
-	//private MineverseChat plugin;
-	
-	/*public Localization() {
-		this.plugin = MineverseChat.getInstance();
-		localizationFile = new File(plugin.getDataFolder().getAbsolutePath(), "en_default.yml");
-		if(!localizationFile.exists()) {
-			try {
-				Files.copy(plugin.getResource("en_default.yml"), localizationFile.toPath());
-			}
-			catch(IOException exception) {
-				exception.printStackTrace();
-			}
-		}
-		localization = YamlConfiguration.loadConfiguration(localizationFile);
-	}
-	
-	public Localization(String name) {	
-		this.plugin = MineverseChat.getInstance();
-		localizationFile = new File(plugin.getDataFolder().getAbsolutePath(), name + ".yml");
-		if(!localizationFile.exists()) {
-			new Localization();
-			return;		
-		}
-		localization = YamlConfiguration.loadConfiguration(localizationFile);
-	}
-	
-	public FileConfiguration getLocalizedMessages() {		
-		return localization;
-	}*/
-	
 	private static MineverseChat plugin;
 	private static FileConfiguration localization;
 	private static File localizationFile;
