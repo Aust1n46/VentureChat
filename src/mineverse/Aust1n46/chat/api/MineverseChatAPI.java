bd6c5d0987332d543b190e398cfafdcd9db78de7
Aust1n46 <aust1n46@yahoo.com>
2021-02-12 15:27:43 -0600

    Added enhanced exception checks and documentation.

--- src/mineverse/Aust1n46/chat/api/MineverseChatAPI.java
+++ src/mineverse/Aust1n46/chat/api/MineverseChatAPI.java
@@ -2,72 +2,157 @@ package mineverse.Aust1n46.chat.api;
 
 import java.util.UUID;
 
+import org.bukkit.entity.Player;
+
 import mineverse.Aust1n46.chat.MineverseChat;
 import mineverse.Aust1n46.chat.bungee.MineverseChatBungee;
 
-import org.bukkit.entity.Player;
-
-//Beginning of the MineverseChat API, this class contains methods to obtain access to a MineverseChatPlayer using either
-//a player pointer, a UUID, or a name in the form of a string.
+/**
+ * API class for looking up wrapped {@link MineverseChatPlayer} objects from
+ * {@link Player}, {@link UUID}, or {@link String} user names.
+ * 
+ * @author Aust1n46
+ */
 public final class MineverseChatAPI {
+
+	/**
+	 * Get a MineverseChatPlayer wrapper from a Bukkit Player instance.
+	 * 
+	 * @param player
+	 *            {@link Player} object.
+	 * @return {@link MineverseChatPlayer}
+	 */
 	public static MineverseChatPlayer getMineverseChatPlayer(Player player) {
-		for(MineverseChatPlayer mcp : MineverseChat.players) {
-			if(mcp.getName().equals(player.getName())) {
-				return mcp;
+		for (MineverseChatPlayer mcp : MineverseChat.players) {
+			try {
+				if (mcp.getName().equals(player.getName())) {
+					return mcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Get a MineverseChatPlayer wrapper from a UUID.
+	 * 
+	 * @param uuid
+	 *            {@link UUID}.
+	 * @return {@link MineverseChatPlayer}
+	 */
 	public static MineverseChatPlayer getMineverseChatPlayer(UUID uuid) {
-		for(MineverseChatPlayer mcp : MineverseChat.players) {
-			if(mcp.getUUID().toString().equals(uuid.toString())) {
-				return mcp;
+		for (MineverseChatPlayer mcp : MineverseChat.players) {
+			try {
+				if (mcp.getUUID().toString().equals(uuid.toString())) {
+					return mcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Get a MineverseChatPlayer wrapper from a user name.
+	 * 
+	 * @param name
+	 *            {@link String}.
+	 * @return {@link MineverseChatPlayer}
+	 */
 	public static MineverseChatPlayer getMineverseChatPlayer(String name) {
-		for(MineverseChatPlayer mcp : MineverseChat.players) {
-			if(mcp.getName().equalsIgnoreCase(name)) {
-				return mcp;
+		for (MineverseChatPlayer mcp : MineverseChat.players) {
+			try {
+				if (mcp.getName().equalsIgnoreCase(name)) {
+					return mcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Get a MineverseChatPlayer wrapper from a Bukkit Player instance. Only checks
+	 * current online players. Much more efficient!
+	 * 
+	 * @param player
+	 *            {@link Player} object.
+	 * @return {@link MineverseChatPlayer}
+	 */
 	public static MineverseChatPlayer getOnlineMineverseChatPlayer(Player player) {
-		for(MineverseChatPlayer mcp : MineverseChat.onlinePlayers) {
-			if(mcp.getName().equals(player.getName())) {
-				return mcp;
+		for (MineverseChatPlayer mcp : MineverseChat.onlinePlayers) {
+			try {
+				if (mcp.getName().equals(player.getName())) {
+					return mcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Get a MineverseChatPlayer wrapper from a UUID. Only checks current online
+	 * players. Much more efficient!
+	 * 
+	 * @param uuid
+	 *            {@link UUID}.
+	 * @return {@link MineverseChatPlayer}
+	 */
 	public static MineverseChatPlayer getOnlineMineverseChatPlayer(UUID uuid) {
-		for(MineverseChatPlayer mcp : MineverseChat.onlinePlayers) {
-			if(mcp.getUUID().toString().equals(uuid.toString())) {
-				return mcp;
+		for (MineverseChatPlayer mcp : MineverseChat.onlinePlayers) {
+			try {
+				if (mcp.getUUID().toString().equals(uuid.toString())) {
+					return mcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Get a MineverseChatPlayer wrapper from a user name. Only checks current
+	 * online players. Much more efficient!
+	 * 
+	 * @param name
+	 *            {@link String}.
+	 * @return {@link MineverseChatPlayer}
+	 */
 	public static MineverseChatPlayer getOnlineMineverseChatPlayer(String name) {
-		for(MineverseChatPlayer mcp : MineverseChat.onlinePlayers) {
-			if(mcp.getName().equalsIgnoreCase(name)) {
-				return mcp;
+		for (MineverseChatPlayer mcp : MineverseChat.onlinePlayers) {
+			try {
+				if (mcp.getName().equalsIgnoreCase(name)) {
+					return mcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Get a SynchronizedMineverseChatPlayer from a UUID.
+	 * 
+	 * @param uuid
+	 *            {@link UUID}
+	 * @return {@link SynchronizedMineverseChatPlayer}
+	 */
 	public static SynchronizedMineverseChatPlayer getSynchronizedMineverseChatPlayer(UUID uuid) {
-		for(SynchronizedMineverseChatPlayer smcp : MineverseChatBungee.players) {
-			if(smcp.getUUID().toString().equals(uuid.toString())) {
-				return smcp;
+		for (SynchronizedMineverseChatPlayer smcp : MineverseChatBungee.players) {
+			try {
+				if (smcp.getUUID().toString().equals(uuid.toString())) {
+					return smcp;
+				}
+			} catch (Exception exception) {
+				continue;
 			}
 		}
 		return null;
